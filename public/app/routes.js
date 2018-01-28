8490d4f2edda145a11330e90641b906baf2bf858
oisinfoley <oisinfoley@yahoo.co.uk>
2018-01-28 22:07:07 +0100

    We were about to have code-redundency, then found a way to detect current state from within our controller. This will allow us to adhere to DRY, and make appropriate API url call based on whether dealing with synonym or antonym.

--- public/app/routes.js
+++ public/app/routes.js
@@ -37,14 +37,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 
 			resolve: {		            
 	            wordsList: function(Word) {		            	
-	            	return Word.listWords();
-	            	
-	            	// var dog = { baseWord:'dog' };
-	            	// $http.post('/api/word/findSynonym', dog).then(function(data){
-	            	// 	console.log("dnd");
-	            	// 	console.log(data.data.word[0].synonyms);
-	            	// 	return "hihi";
-	            	// });
+	            	return Word.listWords();	            	
 	            }
 	            
 	        }		        
@@ -52,22 +45,22 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 
   		.state('thesaurus.synonyms', {
             url: '/synonyms',
-            templateUrl: 'app/views/pages/thesaurus/thesaurus-synonyms.html'//, 
-            // controllerAs:'thesaurus'           
+            templateUrl: 'app/views/pages/thesaurus/thesaurus-synonyms.html', 
+            controller: 'thesaurusSynonymsCtrl',
+			controllerAs: 'thesaurus'
         })
 
-        .state('thesaurus.synonyms.searchResult', {
-            url: '/synonyms/searchResult',
-            templateUrl: 'app/views/pages/thesaurus/search-result.html',            
-        })
+        // .state('thesaurus.synonyms.searchResult', {
+        //     url: '/synonyms/searchResult',
+        //     templateUrl: 'app/views/pages/thesaurus/search-result.html',            
+        // })
         
         // nested list with custom controller
         .state('thesaurus.antonyms', {
             url: '/antonyms',
             templateUrl: 'app/views/pages/thesaurus/thesaurus-antonyms.html',
-            controller: function($scope) {
-                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-            }
+            controller: 'thesaurusAntonymsCtrl',
+			controllerAs: 'thesaurus'
         })
                        
         .state('home', {
@@ -111,42 +104,197 @@ routerApp.directive('wordsLookup', function() {
       
 });
 
-// angular.module('thesaurusController',['thesaurusServices'])
-routerApp.controller('thesaurusCtrl' ,function($scope, Word, wordsList, $state, $http,$timeout) {
-
-// .controller('thesaurusCtrl',function($scope, Word, $timeout){
-
-	console.log("chicken wings");
+routerApp.controller('thesaurusAntonymsCtrl' ,function($scope, Word, wordsList, $state, $http,$timeout) {
 
 	var app = this;
-	//app.loading = true;
 
-	app.loadAllWords = function(){
-			// app.loading = true;
+	app.findSynonym = function(query, wordData, Word, findSynonym){
 
-			console.log("i want to load words...");
+		app.loading = true;
+		console.log("pre POST word data is ::" + app.wordData);		
+		console.log("trying to find synonyms");
 
-			Word.listWords().then(function(data){
+		var parammm = {baseWord: 'dog'};
+		// $state.go('search', {query: 'do'}, {notify:false});
+		// $state.go('search', {query: 'do'}, {notify:false}).then(function(data){
+		// 	console.log("hi hi hi");
+		// 	console.log("hi hi hi ::" + JSON.stringify(data));
+		
+		//our app will not break without this check for undefined, but it's good practice
+		if(app.wordData != undefined){
+		// 	//call our factory service
+
+		// $state.go('search', {query: query});
+		//console.log(Word);
+
+			// Word.findSynonym(app.wordData).then(function(data){
+			$http.post('/api/word/findSynonym', app.wordData).then(function(data){
 				if(data.data.success){
-					//lost scope of controller's 'this' keyword, hence use of var app
-					//app.loading = false;
-					// console.log("the response is :: %s",JSON.stringify(data.data.words));
 
-					// app.wordsList = data.data.words;
-					console.log("words is :: %s", JSON.stringify(data.data.words));
+					// app.loading = false;
+					//timeout only used for demonstration, as response may be too quick to see spinner animation on high-speed connection
+					$timeout(function(){
+						 app.loading = false;
+					}, 500);
 
-					//app.successMsg = data.data.message;
-					// app.errorMsg = data.data.message;
+					console.log("the FINDSYNONYMS SYNOYNMS ARE :: %S",data.data.word[0].synonyms)
+					
+					//we are returned an array from the database, so even though we will usually get only one index,
+					//we must specifiy the zeroth index to access our synonyms
+					console.log(JSON.stringify(data.data.word[0].synonyms));
+
+					app.synonymMsg = data.data.message;
+					app.reminderMsg = data.data.reminder;
+
+					app.synonyms = data.data.word[0].synonyms;
+					;
+
 				}
 				else{
-					console.log("nope");
-					//app.loading = false;
-					// app.errorMsg = data.data.message;
+					app.loading = false;
+					app.synonyms = false;
+					app.synonymMsg = data.data.message;
+
 				}
 			});
-	}
-	$scope.wordsList = wordsList.data.words;
-	console.log($scope.wordsList);
+		} else {
+			//error message created due to wordForm.$valid's value in our thesaurus view
+			app.loading = false;
+			app.errorMsg = 'Please ensure form is filled out properly';
+		}
+	};
+
+	app.addSynonym = function(wordData,valid){
+		console.log("in the addSynonym function");
+
+		var baseWord = [];
+		baseWord.push(app.wordData.baseWord);
+		var synonyms = app.wordData.synonym.split(',');
+		var words = baseWord.concat(synonyms);
+		console.log(words);
+		console.log(words.length);
+		//if user tries to enter empty String, we will catch it here
+		//eg - [a,,,b,,c,,,,,d] becomes [a,b,c,d]
+		words=words.filter(Boolean);
+		console.log("Empty Strings removed!");
+		console.log(words);
+		console.log(words.length);
+
+		//and new code
+		//put result into an object
+		var dictionary = {};
+		for (var i = 0, w; w = words[i]; ++i) {
+		  //take each word (w)
+		  dictionary[w] = words.filter(function(word) {
+		    return word != w;//all words except w
+		  });
+		}
+		//take the object
+		console.log(dictionary);
+		//or stringify it
+		console.log(JSON.stringify(dictionary));
+
+		app.loading = true;
+		//console.log(app.wordData);
+		if(valid){
+			//trying to remove old server messages if user sends a second POST request but hasn't refreshed from the first POST
+			app.successMsg = false;
+			app.errorMsg = false;
+
+			//console.log(app.wordData);
+			// Word.addSynonym(app.wordData).then(function(data, wordData){
+			// Word.addSynonym(dictionary).then(function(data, wordData){
+			$http.post('/api/word/addAntonym', dictionary).then(function(data, wordData){						
+
+				if(data.data.success){
+					//lost scope of 'this' in here, hence use of var app
+					app.loading = false;
+					app.wordData = null;
+						//this is the correct way to clear form, but isn't performing as hoped
+						// $scope.regForm.$setPristine();
+						// $scope.regForm.$setUntouched();
+
+					console.log(data.data.message);
+
+					app.successMsg = data.data.message;
+				}
+				else{
+					app.loading = false;
+					app.errorMsg = data.data.message;
+				}
+			});
+
+		} else {
+			//error message created due to wordForm.$valid's value in our thesaurus view
+			app.loading = false;
+			app.errorMsg = 'Please ensure form is filled out properly';
+		}
+	};
+});
+
+routerApp.controller('thesaurusSynonymsCtrl' ,function($scope, Word, wordsList, $state, $http,$timeout) {
+
+	var app = this;
+
+	app.findSynonym = function(query, wordData, Word, findSynonym){
+
+		console.log($state.current);
+		console.log($state.current.name);
+
+
+		app.loading = true;
+		console.log("pre POST word data is ::" + app.wordData);		
+		console.log("trying to find synonyms");
+
+		var parammm = {baseWord: 'dog'};
+		// $state.go('search', {query: 'do'}, {notify:false});
+		// $state.go('search', {query: 'do'}, {notify:false}).then(function(data){
+		// 	console.log("hi hi hi");
+		// 	console.log("hi hi hi ::" + JSON.stringify(data));
+		
+		//our app will not break without this check for undefined, but it's good practice
+		if(app.wordData != undefined){
+		// 	//call our factory service
+
+		// $state.go('search', {query: query});
+		//console.log(Word);
+
+			// Word.findSynonym(app.wordData).then(function(data){
+			$http.post('/api/word/findSynonym', app.wordData).then(function(data){
+				if(data.data.success){
+
+					// app.loading = false;
+					//timeout only used for demonstration, as response may be too quick to see spinner animation on high-speed connection
+					$timeout(function(){
+						 app.loading = false;
+					}, 500);
+
+					console.log("the FINDSYNONYMS SYNOYNMS ARE :: %S",data.data.word[0].synonyms)
+					
+					//we are returned an array from the database, so even though we will usually get only one index,
+					//we must specifiy the zeroth index to access our synonyms
+					console.log(JSON.stringify(data.data.word[0].synonyms));
+
+					app.synonymMsg = data.data.message;
+					app.reminderMsg = data.data.reminder;
+
+					app.synonyms = data.data.word[0].synonyms;
+					;
+
+				}
+				else{
+					app.loading = false;
+					app.synonyms = false;
+					app.synonymMsg = data.data.message;
+
+				}
+			});
+		} else {
+			//error message created due to wordForm.$valid's value in our thesaurus view
+			app.loading = false;
+			app.errorMsg = 'Please ensure form is filled out properly';
+		}
+	};
 
 	app.addSynonym = function(wordData,valid){
 		console.log("in the addSynonym function");
@@ -214,72 +362,44 @@ routerApp.controller('thesaurusCtrl' ,function($scope, Word, wordsList, $state, 
 			app.errorMsg = 'Please ensure form is filled out properly';
 		}
 	};
+});
 
+// angular.module('thesaurusController',['thesaurusServices'])
+routerApp.controller('thesaurusCtrl' ,function($scope, Word, wordsList, $state, $http,$timeout) {
 
-	//no need for 'valid' like in app.addSynonym() as 'valid' is not initialised until ng-submit executes.
-	//the client will send undefined if the regex fails	which is why we check that wordData != undefined
-	app.findSynonym = function(query, wordData, Word, findSynonym){
+// .controller('thesaurusCtrl',function($scope, Word, $timeout){
 
-		app.loading = true;
-		console.log("pre POST word data is ::" + app.wordData);		
-		console.log("trying to find synonyms");
+	console.log("chicken wings");
 
-		var parammm = {baseWord: 'dog'};
-		// $state.go('search', {query: 'do'}, {notify:false});
-// $state.go('search', {query: 'do'}, {notify:false}).then(function(data){
-// 	console.log("hi hi hi");
-// 	console.log("hi hi hi ::" + JSON.stringify(data));
-		
-		//our app will not break without this check for undefined, but it's good practice
-		if(app.wordData != undefined){
-		// 	//call our factory service
+	var app = this;
+	//app.loading = true;
 
-		// $state.go('search', {query: query});
-		//console.log(Word);
+	app.loadAllWords = function(){
+			// app.loading = true;
 
-			// Word.findSynonym(app.wordData).then(function(data){
-			$http.post('/api/word/findSynonym', app.wordData).then(function(data){
+			console.log("i want to load words...");
+
+			Word.listWords().then(function(data){
 				if(data.data.success){
+					//lost scope of controller's 'this' keyword, hence use of var app
+					//app.loading = false;
+					// console.log("the response is :: %s",JSON.stringify(data.data.words));
 
-					// app.loading = false;
-					//timeout only used for demonstration, as response may be too quick to see spinner animation on high-speed connection
-					$timeout(function(){
-						 app.loading = false;
-					}, 500);
+					// app.wordsList = data.data.words;
+					console.log("words is :: %s", JSON.stringify(data.data.words));
 
-					console.log("the FINDSYNONYMS SYNOYNMS ARE :: %S",data.data.word[0].synonyms)
-					
-					//we are returned an array from the database, so even though we will usually get only one index,
-					//we must specifiy the zeroth index to access our synonyms
-					console.log(JSON.stringify(data.data.word[0].synonyms));
-
-					app.synonymMsg = data.data.message;
-					app.reminderMsg = data.data.reminder;
-
-					app.synonyms = data.data.word[0].synonyms;
-					;
-
+					//app.successMsg = data.data.message;
+					// app.errorMsg = data.data.message;
 				}
 				else{
-					app.loading = false;
-					app.synonyms = false;
-					app.synonymMsg = data.data.message;
-
+					console.log("nope");
+					//app.loading = false;
+					// app.errorMsg = data.data.message;
 				}
 			});
-		} else {
-			//error message created due to wordForm.$valid's value in our thesaurus view
-			app.loading = false;
-			app.errorMsg = 'Please ensure form is filled out properly';
-		}
-	};
-	// app.synonyms = findSynonym.data.word[0].synonyms;
-	// $scope.findSynonym = findSynonym.data;
-	// $scope.findSynonym = findSynonysss.data;
-
-	// console.log(findSynonym.data);
-
-
+	}
+	$scope.wordsList = wordsList.data.words;
+	console.log($scope.wordsList);
 
 });
 
